9c6902ed7b6587eb6c2fc0a676d5313fd3f0638f
Roland <rollycrompton@gmail.com>
2025-01-28 16:34:31 +0000

    switched to altassian from jira

--- jira_notify.py
+++ jira_notify.py
@@ -1,18 +1,22 @@
 import os
-from os import access
-
 import boto3
-import jira
+
+from atlassian import Jira
 
 from dotenv import load_dotenv
 
 load_dotenv()
 queue = os.getenv("MID_PRIORITY_QUEUE")
-access_id = os.getenv("ACCESS_ID")
-access_key = os.getenv("ACCESS_KEY")
+access_id = os.getenv("AWS_ACCESS_KEY_ID")
+access_key = os.getenv("AWS_SECRET_ACCESS_KEY")
+aws_region = os.getenv("AWS_REGION")
+jira_board = os.getenv("JIRA_BOARD")
+jira_token = os.getenv("API_TOKEN")
+
+jira = Jira(url=jira_board, token=jira_token)
 
 sqs = boto3.client("sqs",
-                   region_name="us-east-1",
+                   region_name=aws_region,
                    aws_access_key_id=access_id,
                    aws_secret_access_key=access_key)
 
