adc29d3a3a2a7361bff12e423ac65f52818cf18d
Roland <rollycrompton@gmail.com>
2025-03-07 11:59:41 +0000

    Now using latest version of abstract queue

--- jira_consumer/jira_notify.py
+++ jira_consumer/jira_notify.py
@@ -17,7 +17,7 @@ headers["Authorization"] = f"Bearer {jira_token}"
 exception = exceptions.JIRAError
 class JiraConsumer(AbstractConsumer):
     def __init__(self):
-        super().__init__()
+        super().__init__(queue_var_name="MEDIUM_PRIORITY_QUEUE")
         self.exception = exception
         self.jira = None
 
